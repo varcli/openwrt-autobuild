af29e577f025fefe77ae1cfe10cc8ebb4f1ca19d
chenli <chenli@zzjspd.com>
2025-07-22 10:30:04 +0800

    update

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -92,7 +92,7 @@ uci commit
 
 # 设置编译作者信息
 FILE_PATH="/etc/openwrt_release"
-NEW_DESCRIPTION="Compiled by GEOMCH"
+NEW_DESCRIPTION="Compiled by geomch"
 sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='$NEW_DESCRIPTION'/" "$FILE_PATH"
 
 exit 0
