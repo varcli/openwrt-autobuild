1a4f132294a3141b39a582a146017121ddc876c7
varcli <i@varcli.com>
2026-06-03 22:43:20 +0800

    feat: 同步第三方仓库并改用ipk包

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -13,17 +13,18 @@ if [ -z "$CUSTOM_PACKAGES" ]; then
   echo "⚪️ 未选择 任何第三方软件包"
 else
   # ============= 同步第三方插件库==============
-  # 同步第三方软件仓库run/apk
+  # 同步第三方软件仓库wukongdaily run/ipk
   echo "🔄 正在同步第三方软件仓库 Cloning run file repo..."
-  git clone --depth=1 https://github.com/wukongdaily/apk.git /tmp/store-apk-repo
+  git clone --depth=1 https://github.com/wukongdaily/store.git /tmp/store-run-repo
 
-  # 拷贝 run/x86 下所有 run 文件和apk文件 到 extra-packages 目录
+  # 拷贝 run/x86 下所有 run 文件和ipk文件 到 extra-packages 目录
   mkdir -p /home/build/immortalwrt/extra-packages
-  cp -r /tmp/store-apk-repo/run/x86/* /home/build/immortalwrt/extra-packages/
+  cp -r /tmp/store-run-repo/run/x86/* /home/build/immortalwrt/extra-packages/
 
   echo "✅ Run files copied to extra-packages:"
-  # 解压并拷贝apk到packages目录
-  sh shell/apk-prepare-packages.sh
+  ls -lh /home/build/immortalwrt/extra-packages/*.run
+  # 解压并拷贝ipk到packages目录
+  sh shell/prepare-packages.sh
   ls -lah /home/build/immortalwrt/packages/
 fi
 
